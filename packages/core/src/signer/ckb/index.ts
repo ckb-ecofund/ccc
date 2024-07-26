8a7fa6e968d2cd516017dc7c9a7c8407ca201499
SpectreMercury <francislee920217@gmail.com>
2024-07-27 02:14:33 +0800

    fix(format): fix code format

--- packages/core/src/signer/ckb/index.ts
+++ packages/core/src/signer/ckb/index.ts
@@ -1,5 +1,6 @@
 export * from "./signerCkbPrivateKey";
 export * from "./signerCkbPublicKey";
 export * from "./signerCkbScriptReadonly";
+export * from "./signerMnemonicPhrase";
 export * from "./verifyJoyId";
 export * from './signerMnemonicPhrase';
