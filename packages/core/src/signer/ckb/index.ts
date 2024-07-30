57590f1a61f6ecd6deb1517967ec56ae7a1fd9a4
Hanssen0 <hanssen0@hanssen0.com>
2024-07-31 02:27:21 +0800

    fix(core): commonjs support
    remove JoyID signature verification temporary to avoid ckb sdk js

--- packages/core/src/signer/ckb/index.ts
+++ packages/core/src/signer/ckb/index.ts
@@ -2,4 +2,3 @@ export * from "./signerCkbPrivateKey.js";
 export * from "./signerCkbPublicKey.js";
 export * from "./signerCkbScriptReadonly.js";
 export * from "./verifyCkbSecp256k1.js";
-export * from "./verifyJoyId.js";
