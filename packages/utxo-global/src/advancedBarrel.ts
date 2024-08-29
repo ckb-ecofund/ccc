278ca6c2d12d4aa42415343a92c450139463d8a5
Trong Dinh <trongdth@gmail.com>
2024-08-29 10:20:39 +0700

    remove createTx

--- packages/utxo-global/src/advancedBarrel.ts
+++ packages/utxo-global/src/advancedBarrel.ts
@@ -5,7 +5,6 @@ export interface Provider {
   connect(): Promise<void>;
   isConnected(): Promise<boolean>;
   signMessage(msg: string, address: string): Promise<string>;
-  createTx(tx: any): Promise<string>;
   signTransaction(tx: any): Promise<any>;
   getNetwork(): Promise<string>;
   switchNetwork(network: string): Promise<void>;
