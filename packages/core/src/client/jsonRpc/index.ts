fad67d00974bf072879a2b6a5fbe04c9cfdde403
Hanssen0 <hanssen0@hanssen0.com>
2024-09-22 01:12:49 +0800

    feat(core): readable client error

--- packages/core/src/client/jsonRpc/index.ts
+++ packages/core/src/client/jsonRpc/index.ts
@@ -420,7 +420,7 @@ export abstract class ClientJsonRpc extends Client {
       throw new Error(`Id mismatched, got ${res.id}, expected ${payload.id}`);
     }
     if (res.error) {
-      throw res.error;
+      throw new Error(`Client request error ${JSON.stringify(res.error)}`);
     }
     return res.result;
   }
