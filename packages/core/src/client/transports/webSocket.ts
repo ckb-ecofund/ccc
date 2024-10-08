00158538fe15818d5bc3271719c88164890e3e27
Hanssen0 <hanssen0@hanssen0.com>
2024-10-09 00:14:13 +0800

    fix(core): close WebSocket on timeout

--- packages/core/src/client/transports/webSocket.ts
+++ packages/core/src/client/transports/webSocket.ts
@@ -18,54 +18,59 @@ export class TransportWebSocket implements Transport {
   ) {}
 
   request(data: JsonRpcPayload) {
-    const socket = (() => {
-      if (this.socket) {
+    const socket = (this.socket ?? Promise.resolve(undefined)).then(
+      (existed) => {
+        if (existed && existed.readyState === existed.OPEN) {
+          return existed;
+        }
+
+        const socket = new WebSocket(this.url);
+        const onMessage = ({ data }: { data: string }) => {
+          const res = JSON.parse(data);
+          if (typeof res !== "object" || res === null) {
+            throw new Error(`Unknown response ${data}`);
+          }
+
+          const req = this.ongoing.get(res.id);
+          if (!req) {
+            return;
+          }
+          clearTimeout(req[2]);
+          this.ongoing.delete(res.id);
+
+          req[0](res);
+        };
+        const onClose = () => {
+          this.socket = undefined;
+          this.ongoing.forEach(([_, onError]) =>
+            onError(new Error("Connection closed")),
+          );
+          this.ongoing.clear();
+        };
+
+        socket.onclose = onClose;
+        socket.onerror = onClose;
+        socket.onmessage = onMessage;
+
+        this.socket = new Promise((resolve) => {
+          if (socket.readyState === socket.OPEN) {
+            resolve(undefined);
+          } else {
+            socket.onopen = resolve;
+          }
+        }).then(() => socket);
         return this.socket;
-      }
-
-      const socket = new WebSocket(this.url);
-      const onMessage = ({ data }: { data: string }) => {
-        const res = JSON.parse(data);
-        if (typeof res !== "object" || res === null) {
-          throw new Error(`Unknown response ${data}`);
-        }
-
-        const req = this.ongoing.get(res.id);
-        if (!req) {
-          return;
-        }
-        clearTimeout(req[2]);
-        this.ongoing.delete(res.id);
-
-        req[0](res);
-      };
-      const onClose = (error?: unknown) => {
-        this.socket = undefined;
-        this.ongoing.forEach(([_, onError]) =>
-          onError(error ?? new Error("Connection closed")),
-        );
-        this.ongoing.clear();
-      };
-
-      socket.onclose = onClose;
-      socket.onerror = onClose;
-      socket.onmessage = onMessage;
-
-      this.socket = new Promise((resolve) => {
-        if (socket.readyState === socket.CONNECTING) {
-          socket.onopen = resolve;
-        } else {
-          resolve(undefined);
-        }
-      }).then(() => socket);
-      return this.socket;
-    })();
+      },
+    );
 
     return new Promise((resolve, reject) => {
       this.ongoing.set(data.id, [
         resolve,
         reject,
-        setTimeout(() => reject(new Error("Request timeout")), this.timeout),
+        setTimeout(() => {
+          socket.then((socket) => socket.close());
+          reject(new Error("Request timeout"));
+        }, this.timeout),
       ]);
       socket.then((socket) => {
         if (
