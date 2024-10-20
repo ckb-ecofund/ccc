6825b97f15fb1d2b85c9c2ab7e49b9db250c19a8
homura <homura.dev@gmail.com>
2024-10-20 23:19:13 +0900

    chore: setup test env

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,21 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["<rootDir>/packages/*/**/*.test.ts"],
+  transform: {
+    "^.+.[jt]s$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+        useESM: true,
+        isolatedModules: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
